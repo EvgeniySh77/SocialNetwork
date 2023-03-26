1c68030c3e3ea9eecc1f6383454695886082baee
evgen <evgen@FESKO>
2023-03-26 23:56:06 +0300

    Добавьте файлы проекта.

---
+++ SocialNetworkTest/BLL/Services/FriendRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+
+namespace SocialNetworkTest.BLL.Services
+{
+    [TestClass()]
+    public class UserAddingFriendDataTests
+    {
+        UserService UserService = new UserService();
+        [TestMethod()]
+        public void UserNotFoundException()
+        {
+            UserAddingFriendData userAddingFriendData = new UserAddingFriendData();
+            Assert.ThrowsException<UserNotFoundException>(() => UserService.AddFriend(userAddingFriendData));
+        }        
+    }
+}
